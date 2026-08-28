677c1cecee2041bc18c52b6c57ed10a6a76c6e0a
nuinik <88400823+nuinik@users.noreply.github.com>
2026-08-28 21:12:47 +0200

    Refactor Kontinent class for improved formatting
    Formatted the Kontinent class for better readability.

--- src/main/java/hr/algebra/drzave/entity/Kontinent.java
+++ src/main/java/hr/algebra/drzave/entity/Kontinent.java
@@ -1,9 +1,32 @@
 package hr.ispit.drzave.entity;
 import jakarta.persistence.*;
-@Entity public class Kontinent {
- @Id @GeneratedValue(strategy=GenerationType.IDENTITY) private Long id;
- @Column(nullable=false,unique=true) private String naziv;
- public Kontinent(){} public Kontinent(String naziv){this.naziv=naziv;}
- public Long getId(){return id;} public String getNaziv(){return naziv;} public void setNaziv(String naziv){this.naziv=naziv;}
+
+@Entity 
+ public class Kontinent {
+  
+ @Id 
+ @GeneratedValue(strategy=GenerationType.IDENTITY) 
+ private Long id;
+  
+ @Column(nullable=false,unique=true) 
+ private String naziv;
+ public Kontinent(){
+ } 
+  
+ public Kontinent(String naziv){
+  this.naziv=naziv;
+ }
+  
+ public Long getId(){
+  return id;
+ } 
+  
+ public String getNaziv(){
+  return naziv;
+ } 
+  
+ public void setNaziv(String naziv){
+  this.naziv=naziv;
+ }
 }
 
