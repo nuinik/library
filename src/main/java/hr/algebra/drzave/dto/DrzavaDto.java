b7f0f98dc7041f959e04909c3262e216b3dfa1d0
nuinik <88400823+nuinik@users.noreply.github.com>
2026-08-28 21:47:29 +0200

    Update DrzavaDto.java

--- src/main/java/hr/algebra/drzave/dto/DrzavaDto.java
+++ src/main/java/hr/algebra/drzave/dto/DrzavaDto.java
@@ -1,3 +1,26 @@
 package hr.ispit.drzave.dto;
-public record DrzavaDto(Long id,String naziv,long brojStanovnika,Long kontinentId,String kontinent) {}
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class DrzavaDTO {
+
+    private Long drzavaID;
+
+    @NotBlank(message = "Naziv države je obavezan")
+    @Size(max = 50, message = "Naziv države ne može imati više od 50 znakova")
+    private String naziv;
+
+    @NotNull(message = "Broj stanovnika je obavezan")
+    @Min(value = 0, message = "Broj stanovnika ne može biti negativan")
+    private Long brojStanovnika;
+
+    @NotNull(message = "ID kontinenta je obavezan")
+    private Long continentID;
+
+    private String continentNaziv;
+}
