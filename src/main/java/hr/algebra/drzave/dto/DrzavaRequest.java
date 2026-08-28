38b8a6b523a18f0590e5a920ac3cfbe376a124f1
nuinik <88400823+nuinik@users.noreply.github.com>
2026-08-28 21:48:14 +0200

    Clean up DrzavaRequest by removing dead code

--- src/main/java/hr/algebra/drzave/dto/DrzavaRequest.java
+++ src/main/java/hr/algebra/drzave/dto/DrzavaRequest.java
@@ -1,4 +1,1 @@
-package hr.ispit.drzave.dto;
-import jakarta.validation.constraints.*;
-public record DrzavaRequest(@NotBlank String naziv, @Positive long brojStanovnika, @NotNull Long kontinentId) {}
-
+DEAD CODE
